b6c7fef39a418e874b2300ff26f3ad2ac9ea8962
Philipp Staender <pstaender@mailbox.org>
2025-07-04 20:55:14 +0200

    Adds SilverStripe v6 compatibilty (#13)
    * increases versions of php modules
    * fixes ci
    * uses sake console application to make sake available in the REPL

--- src/SSShell/SakeCommand.php
+++ src/SSShell/SakeCommand.php
@@ -3,9 +3,12 @@
 namespace PStaender\SSShell;
 
 use Psy\Command\Command;
+use SilverStripe\Cli\Sake;
 use SilverStripe\Control\CLIRequestBuilder;
 use SilverStripe\Control\HTTPApplication;
 use SilverStripe\Core\CoreKernel;
+use SilverStripe\ORM\DB;
+use Symfony\Component\Console\Input\ArgvInput;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -15,17 +18,45 @@ class SakeCommand extends Command
     protected function configure()
     {
         $this
-            ->setDescription('Run a sake command, e.g. sake dev/build')
-            ->addArgument('url', InputArgument::REQUIRED);
+            ->setDescription('Run a sake command, e.g. sake db:build')
+            ->addOption('verbose', ['v', 'vv', 'vvv'])
+            ->addOption('flush', 'f')
+            ->addOption('quiet', 'q')
+            ->addOption('version', 'V')
+            ->addOption('silent')
+            ->addOption('ansi')
+            ->addOption('no-ansi')
+            ->addOption('no-interaction')
+            ->addOption('no-database')
+            ->setHelp('This command allows you to run any sake command from the command line.')
+            ->addArgument('arg', InputArgument::IS_ARRAY, 'The sake command to run, e.g. "db:build" or "dev/tasks/MyTask"');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $url = $input->getArgument('url');
-        return self::execute_silverstripe_url($url ?: '', input: $input, output: $output);
+        DB::setMustUsePrimary();
+        $sake = new Sake();
+        $args = $input->getArguments()["arg"];
+        $options = [
+            $input->getOption('verbose') ? '-vvv' : null,
+            $input->getOption('version') ? '--version' : null,
+            $input->getOption('flush') ? '--flush' : null,
+            $input->getOption('silent') ? '--silent' : null,
+            $input->getOption('quiet') ? '--quiet' : null,
+            $input->getOption('ansi') ? '--ansi' : null,
+            $input->getOption('no-ansi') ? '--no-ansi' : null,
+            $input->getOption('no-interaction') ? '--no-interaction' : null,
+            $input->getOption('no-database') ? '--no-database' : null,
+        ];
+        $options = array_filter($options); // Remove null values
+        $options = array_filter($options, fn($option) => $option !== '--help');
+        $args = array_merge($options, $args);
+        array_unshift($args, 'sake'); // Add 'sake' as the first argument to match the expected command structure
+        $sake->setAutoExit(false);
+        return $sake->run(new ArgvInput($args));
     }
 
-    public static function execute_silverstripe_url($url, InputInterface $input = null, OutputInterface $output = null): int
+    public static function execute_silverstripe_url($url, ?InputInterface $input = null, ?OutputInterface $output = null): int
     {
         // hacky way to force a parameter, but this seems to be the most efficient way here
         $_SERVER['REQUEST_URI'] = $url;
